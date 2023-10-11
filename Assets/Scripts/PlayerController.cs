1ec2490e746d6273fc426f15bed8a4f23aae6b33
Hugo Lapa <Hugolapa888@gmail.com>
2023-10-11 09:50:47 +0100

    play controller 2.2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour {
 
     public Vector2 moveValue;
     public float speed;
+    private int count;
+
+    void Start() {
+        count = 0;
+    }
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
@@ -15,5 +20,12 @@ public class PlayerController : MonoBehaviour {
     void FixedUpdate() {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "PickUp") {
+            other.gameObject.SetActive(false);
+            count += 1; // Increment the count by 1 when a pickup is collected
+        }
     }
 }
